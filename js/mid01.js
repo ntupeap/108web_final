f4bb6db469e1a8cab6cf89094ed6d1ef755c9491
ntusss <>
2020-01-02 11:38:05 +0800

    v3 fadein&out

--- js/mid01.js
+++ js/mid01.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $(window).scroll(function () {
-        $(".slideanim").each(function () {
+        $(".fadeout").each(function () {
             var pos = $(this).offset().top;
             
             var winTop = $(window).scrollTop();
             console.log(pos)
-            if (winTop>(pos*0.9)) {
-                $(this).addClass("slide");
+            if (winTop>(pos*0.7)) {
+                $(this).addClass("fadein");
             }
             else{
-                $(this).removeClass("slide");
+                $(this).removeClass("fadein");
             }
         });
     });
