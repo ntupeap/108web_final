58e261f2f6a1b7854eed9a6734e3cbbdb9900530
ntupeap <ds910258@gmail.com>
2020-01-05 19:50:26 +0800

    v7 page2 0105

--- js/mid01.js
+++ js/mid01.js
@@ -40,6 +40,72 @@ $(document).ready(function () {
     });
 });
 
+//set fix page2
+$(document).ready(function () {
+    var obj_position = $('#page2_son').position().top;
+    var obj_height = $('#page2_son').height();
+    var next_obj_position = $('#page3').position().top;
+    var this_page_height = $('#page2').height();
+    $(window).scroll(function () {
+        var scroll_position = $(window).scrollTop();
+        if ((scroll_position > obj_position) && (scroll_position < next_obj_position - obj_height)) {
+            $("#page2_son").addClass("fix");
+        } else {
+            $("#page2_son").removeClass("fix");
+        }
+
+        if (scroll_position > (next_obj_position - obj_height)) {
+            $("#page2_son").addClass('fix_bottom_page2_son');
+        } else {
+            $("#page2_son").removeClass('fix_bottom_page2_son');
+        }
+        
+        if (scroll_position > (next_obj_position - (4*obj_height))){
+            $("#company_intro").addClass('hidden');
+        } else{
+            $("#company_intro").removeClass('hidden');
+        }
+        
+        if (scroll_position > (next_obj_position - (4*obj_height))){
+            $("#company").removeClass('hidden');
+        } else{
+            $("#company").addClass('hidden');
+        }
+        
+        
+        //company show setting
+        
+        
+        if (scroll_position > (next_obj_position - (3*obj_height))){
+//            $("#B_company").removeClass('hidden');
+            $("#A_company").addClass('hidden');
+        } else{
+            $("#A_company").removeClass('hidden');
+//            $("#B_company").addClass('hidden');
+        }
+        
+        if (scroll_position > (next_obj_position - (2*obj_height))){
+            $("#C_company").removeClass('hidden');
+//            $("#B_company").addClass('hidden');
+        } else{
+//            $("#B_company").removeClass('hidden');
+            $("#C_company").addClass('hidden');
+        }
+        
+        if ((scroll_position > (next_obj_position - (3*obj_height)))&&(scroll_position < (next_obj_position - (2*obj_height)))){
+            $("#B_company").removeClass('hidden');
+        } else{
+            $("#B_company").addClass('hidden');
+        }
+        
+        
+        console.log(scroll_position)
+        
+        console.log(next_obj_position)
+        
+        console.log(obj_height)
+    });
+});
 
 //set fix page3
 $(document).ready(function () {
